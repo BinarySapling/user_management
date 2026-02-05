4feca93ade91b7dabdea93cbd73cc38e397c2a7e
Danish <danishanwarofficial@gmail.com>
2026-02-05 12:19:02 +0530

    feat: add route to retrieve all users

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -5,17 +5,19 @@ import {
     createUser,
     updateUser,
     patchUser,
-    deleteUser
+    deleteUser,
+    getAllUsers
 }
 from "../controllers/user.controller.js"
 import { checkAuth, validateUser , validateUserId } from "../middleware/auth.js";
 
 const router = express.Router();
 
-router.post("/", validateUserlidateUser ,createUser);
+router.post("/", validateUser ,createUser);
 router.get("/:id", validateUserId,getUser);
 router.put("/:id", updateUser);
 router.patch("/:id", patchUser);
 router.delete("/:id", deleteUser);
+router.get("/all",getAllUsers)
 
 export default router;
